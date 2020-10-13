09a281aabc1b463fd3e6760f89f6d2531f152700
nurlatief <latief782@gmail.com>
2020-10-13 03:45:24 -0700

    regex

--- 01-04/latihan/regex.js
+++ 01-04/latihan/regex.js
@@ -1,22 +1,22 @@
-// var foo = /arofah/;
+var foo = /arofah/;
 
-// console.log(foo);  // /ab+c/
-// console.log(typeof foo);  // object
+console.log(foo);  // /ab+c/
+console.log(typeof foo);  // object
 
-// var bar = new RegExp("arofah");
+var bar = new RegExp("arofah");
 
-// console.log(bar);  ///ab+c/
-// console.log(typeof bar);  // object
+console.log(bar);  ///ab+c/
+console.log(typeof bar);  // object
 
-// var foo = "Belajar JavaScript";
-// var pola = /JavaScript/;
+var foo = "Belajar JavaScript";
+var pola = /JavaScript/;
 
-// console.log(pola.test(foo));  // true
-// console.log(/Belajar/.test(foo));  // true
-// console.log(/belajar/.test(foo));  // false
+console.log(pola.test(foo));  // true
+console.log(/Belajar/.test(foo));  // true
+console.log(/belajar/.test(foo));  // false
 
-// var foo = "1 jam sama dengan 60 menit, juga sama dengan 3600 detik";
-// var pola = /\d+/;
+var foo = "1 jam sama dengan 60 menit, juga sama dengan 3600 detik";
+var pola = /\d+/;
 
-// console.log(pola.exec(foo));  // Array ["1"]
+console.log(pola.exec(foo));  // Array ["1"]
 
